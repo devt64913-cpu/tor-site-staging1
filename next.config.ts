3829ca58d8ae126d524b662332f2330cd12727c4
devtesting <devt64913@gmail.com>
2026-02-02 13:11:14 +0000

    config structuring

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export",
+  // output: "export",
+  reactStrictMode: true,
   images: { unoptimized: true },
 };
 
